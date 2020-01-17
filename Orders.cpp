293f7e8e38b8c578fd529670fe1d412c40bc849e
arthur2606 <bissesowe@gmail.com>
2020-01-17 23:42:20 +0100

    1 er commit pour la branche Orders

---
+++ Orders.cpp
@@ -0,0 +1,45 @@
+/*
+Author=ing BISSE SOWE Emmanuel Arthur --*
+Class-Name=Orders
+**declaration des prototypes**
+-->creation des getters pour obtention de données entrées par le User--
+-->creation des setters pour modifications des données--
+*/
+																															
+#include <string>
+#include<iostream>
+#include<ostream>
+#include <vector>
+#include "Client.h"
+#include "Products.h"
+#include "Orders.h"
+
+using namespace std;
+
+
+
+		
+		Orders::Orders(Client client,std::vector<Products> products,std::string statut):m_client(client),m_statut(statut)
+		{
+			for(int i=0;i<products.size();i++)
+			{
+				m_products.at(i)=products.at(i);
+			}
+	
+		}
+		std::string Orders::getStatut() const
+		{
+			return m_statut;
+		}
+		
+		void Orders::UpdateStatut(std::string NewStatut)
+		{
+			m_statut=NewStatut;
+
+		}
+		std::ostream &operator << (std::ostream &output,Orders& O) 
+				{  
+					output <<O.m_client->getPrenom()<<"  "<<O.m_client->getNom()<<"    "<<O.m_products->getTitle()<<"  "<<O.getStatut() <<"   "<<std::endl; 
+					return output; 
+
+			    }
---
+++ Orders.h
@@ -0,0 +1,39 @@
+/*
+Author=ing BISSE SOWE Emmanuel Arthur --*
+Class-Name=Products
+**declaration des prototypes**
+-->creation des getters pour obtention de données entrées par le User--
+-->creation des setters pour modifications des données--
+*/
+
+#ifndef ORDERS_H
+#define ORDERS_H																																	
+#include <string>
+#include<iostream>
+#include<ostream>
+#include <vector>
+
+#include "Products.h"
+
+#include "Client.h"
+
+
+
+class Orders{
+
+	public:
+
+		Orders(Client client,std::vector<Products> products,std::string statut);
+		void UpdateStatut(std::string Statut); //modifier le statut d'une  commande 
+		std::string getStatut() const;
+		friend std::ostream &operator << (std::ostream &output,Orders& O);
+
+						
+	private:
+		Client m_client;
+		std::vector<Products*> m_products;
+		std::string m_statut;
+
+		
+		} ;
+		#endif
