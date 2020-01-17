a93282f8f2c9adb92b0d9e4fbb9ba2bf6ff8fe5f
arthur2606 <bissesowe@gmail.com>
2020-01-17 23:46:37 +0100

    1 er commit pour la branche Products

---
+++ Products.cpp
@@ -0,0 +1,49 @@
+/*
+Author=ing BISSE SOWE Emmanuel Arthur --*
+Class-Name=Products
+**developpement des prototypes**
+-->developpement des getters pour obtention de données entrées par le User--
+-->developpement des setters pour modifications des données--
+*/																																	
+#include <string>
+#include<iostream>
+#include<ostream>
+#include <vector>
+#include "Products.h"
+#include "Client.h"
+
+
+
+using namespace std;
+
+
+		Products::Products(std::string title,std::string description,int Qte,float price)
+		{
+			m_title=title;
+		 	m_description=description;
+			m_Qte=Qte;
+			m_priceU=price;
+
+		}
+		
+		void Products::setQte(int Q)
+		{
+			m_Qte=Q;
+		}
+		std::string Products::getTitle() const
+		{
+			return m_title;
+
+		}
+		std::string Products::getDescription() const
+		{
+			return m_description;
+		}
+		int Products::getQte() const
+		{
+			return m_Qte;
+		}
+		float Products::getprice() const
+		{
+			return m_priceU;
+		}
---
+++ Products.h
@@ -0,0 +1,42 @@
+/*
+Author=ing BISSE SOWE Emmanuel Arthur --*
+Class-Name=Products
+**declaration des prototypes**
+-->creation des getters pour obtention de données entrées par le User--
+-->creation des setters pour modifications des données--
+*/
+
+#ifndef PRODUCTS_H
+#define PRODUCTS_H																																	
+#include <string>
+#include<iostream>
+#include<ostream>
+#include <vector>
+#include "Client.h"
+#include "Orders.h"
+#include "Magasin.h"
+
+
+class Products{
+
+	public:
+		Products(std::string title="PS4",std::string description="Console de jeu de marque Sony",int Qte=2,float price=0.0);
+		std::string getTitle() const;
+		std::string getDescription() const;
+		int getQte() const;
+		float getprice() const;
+		void setQte(int Q);
+		
+		friend std::ostream &operator << (std::ostream &output,Products& P);
+
+	private:
+		std::string m_title;
+		std::string m_description;
+		int m_Qte;
+		float m_priceU;
+		 
+
+
+
+} ;
+#endif
